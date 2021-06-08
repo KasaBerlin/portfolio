918ac8b5ebf1630fb396dfa3a3dcf15f0dbb1ad1
KasaBerlin <katrinsalac@gmail.com>
2021-06-08 22:04:34 +0200

    gitignore update

---
+++ build/precache-manifest.4b2acc195d0b0a4a909df63b95d3b533.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d66ebcaa8f13b5780634340641f93e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "424841562e74d1068871",
+    "url": "/portfolio/static/css/2.1f43c709.chunk.css"
+  },
+  {
+    "revision": "d28fe8edea43f59bf9d0",
+    "url": "/portfolio/static/css/main.a199d57e.chunk.css"
+  },
+  {
+    "revision": "424841562e74d1068871",
+    "url": "/portfolio/static/js/2.d0d4daf2.chunk.js"
+  },
+  {
+    "revision": "d28fe8edea43f59bf9d0",
+    "url": "/portfolio/static/js/main.cee7dbec.chunk.js"
+  },
+  {
+    "revision": "c98fad0fa5c777d28f1b",
+    "url": "/portfolio/static/js/runtime-main.36963274.js"
+  },
+  {
+    "revision": "202bb817a35d5ad96aeaacc3d0c4eff9",
+    "url": "/portfolio/static/media/CV-KatrinSalac.202bb817.pdf"
+  },
+  {
+    "revision": "dcab3f49f7f07a4b56ea3369dd88226a",
+    "url": "/portfolio/static/media/alltag.dcab3f49.png"
+  },
+  {
+    "revision": "38101a9f6b01ab88933b811dc71ae280",
+    "url": "/portfolio/static/media/blog.38101a9f.png"
+  },
+  {
+    "revision": "0425d661f34ffa46604c9dfa344c03bb",
+    "url": "/portfolio/static/media/fa-brands-400.0425d661.woff2"
+  },
+  {
+    "revision": "189c0326f75931886d5b60cde8b5628f",
+    "url": "/portfolio/static/media/fa-brands-400.189c0326.ttf"
+  },
+  {
+    "revision": "3a19eb0134e93ea0caa5826f5fb7b642",
+    "url": "/portfolio/static/media/fa-brands-400.3a19eb01.svg"
+  },
+  {
+    "revision": "451b51d8700fb035bcaaa9e8710b384a",
+    "url": "/portfolio/static/media/fa-brands-400.451b51d8.eot"
+  },
+  {
+    "revision": "85b063debeb5d5f079776ac1a9c924cb",
+    "url": "/portfolio/static/media/fa-brands-400.85b063de.woff"
+  },
+  {
+    "revision": "14f22fbed8146dbf3b497bb32d8491b0",
+    "url": "/portfolio/static/media/fa-regular-400.14f22fbe.woff"
+  },
+  {
+    "revision": "772a0f14c850c8b0dfe283a95857583e",
+    "url": "/portfolio/static/media/fa-regular-400.772a0f14.woff2"
+  },
+  {
+    "revision": "8cd2768111e3091948920a3ad11f96c6",
+    "url": "/portfolio/static/media/fa-regular-400.8cd27681.svg"
+  },
+  {
+    "revision": "9230982faf6ed4d1dab07babfb813777",
+    "url": "/portfolio/static/media/fa-regular-400.9230982f.ttf"
+  },
+  {
+    "revision": "b6579220162705be426ad4db0dc43f6b",
+    "url": "/portfolio/static/media/fa-regular-400.b6579220.eot"
+  },
+  {
+    "revision": "1898ebd2e3162e7ca8627073d865b893",
+    "url": "/portfolio/static/media/fa-solid-900.1898ebd2.eot"
+  },
+  {
+    "revision": "1d825f28ae4e412886fb0123b94a8ac2",
+    "url": "/portfolio/static/media/fa-solid-900.1d825f28.svg"
+  },
+  {
+    "revision": "4cc04a31c42f2f9d951547bbce75960b",
+    "url": "/portfolio/static/media/fa-solid-900.4cc04a31.woff2"
+  },
+  {
+    "revision": "b1161bce5381a5fa7aacc073e1a01dd3",
+    "url": "/portfolio/static/media/fa-solid-900.b1161bce.ttf"
+  },
+  {
+    "revision": "db525089f5101b59a66184d109e6f917",
+    "url": "/portfolio/static/media/fa-solid-900.db525089.woff"
+  },
+  {
+    "revision": "ccd12288a798e29390505594b05b2d49",
+    "url": "/portfolio/static/media/germany.ccd12288.png"
+  },
+  {
+    "revision": "21670d152075caa7de17e57adebafc35",
+    "url": "/portfolio/static/media/guess.21670d15.png"
+  },
+  {
+    "revision": "611a4bcb556f366641c6925c745ad72d",
+    "url": "/portfolio/static/media/rose-blue.611a4bcb.svg"
+  },
+  {
+    "revision": "d5dc20f048958d159d7d298441e91f4a",
+    "url": "/portfolio/static/media/rose-egg.d5dc20f0.svg"
+  },
+  {
+    "revision": "2c3d09f1c9d8aa2c242a03eb56cfe924",
+    "url": "/portfolio/static/media/weatherapp.2c3d09f1.png"
+  }
+]);
